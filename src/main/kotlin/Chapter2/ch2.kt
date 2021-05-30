47fd8ea13607b30e1886f043a017a13ce045ee0d
toppy368 <toppy36849@gmail.com>
2021-05-30 23:08:24 +0800

    分別設定 b、c、e的變數為Boolean、Char、List，並分別顯示出來

--- src/main/kotlin/Chapter2/ch2.kt
+++ src/main/kotlin/Chapter2/ch2.kt
@@ -3,6 +3,14 @@ package Chapter2
 fun main() {
     var a: Int = 5
     var msg: String = "hello"
+    var b: Boolean = false
+    var c: Char = 'f'
+    var e: List<Int> = arrayListOf(1, 2, 5)
+
     println(a)
     println(msg)
+    println(b)
+    println(c)
+    println(e)
+
 }
