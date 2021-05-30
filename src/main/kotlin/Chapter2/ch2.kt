6aba45db89176f469a6a77c01bd50050e1aef61d
toppy368 <toppy36849@gmail.com>
2021-05-30 23:10:36 +0800

    編寫註解，第5~9行為設定變數，第12~16行為顯示變數

--- src/main/kotlin/Chapter2/ch2.kt
+++ src/main/kotlin/Chapter2/ch2.kt
@@ -1,12 +1,14 @@
 package Chapter2
 
 fun main() {
+    //設定變數
     var a: Int = 5
     var msg: String = "hello"
     var b: Boolean = false
     var c: Char = 'f'
     var e: List<Int> = arrayListOf(1, 2, 5)
 
+    //顯示以上變數
     println(a)
     println(msg)
     println(b)
